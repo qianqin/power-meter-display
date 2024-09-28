fcd8446c7381cb75d4e88d49efe8d64776705d72
Qian Qin <mail@qianqin.de>
2024-09-28 17:13:19 +0200

    added new default values

--- src/config_sample.h
+++ src/config_sample.h
@@ -7,14 +7,15 @@
 #define LED_PIN 7 // Pin for LEDs
 #define LED_COUNT 32 // amount LEDs
 #define MAX_POWER LED_COUNT * 50 // max amount in consumption mode
-#define MAX_PRODUCING 600 // max amount in producing mode
+#define MAX_PRODUCING LED_COUNT * 50 // max amount in producing mode
 #define ANIMATE_DELAY 20 // wait ms to turn on next led
 #define POLLING_DELAY 1000 // time to wait after each poll
 #define LED_MAX_BRIGHTNESS_PRODUCING 255 // max value allowed for LEDs when producing
-#define LED_MAX_BRIGHTNESS_CONSUMING 50 // max value allowed for LEDs when not producing
-#define ANIMATE_STEP 4 // increase by x per animation cycle
+#define LED_MAX_BRIGHTNESS_CONSUMING 30 // max value allowed for LEDs when not producing
+#define ANIMATE_STEP 2 // increase by x per animation cycle
 #define LED_CHANGE_DELAY 250 // delay increase and decrease of LEDs
-#define NEOPIXEL_METHOD NeoWs2812xMethod
+#define NEOPIXEL_METHOD NeoEsp32Rmt0Ws2812xMethod //NeoWs2812xMethod
+#define GREE_AC_REMOTE gree_ac_remote_model_t::YBOFB
 #define GREE_AC_PIN 3
 #define GREE_AC_TEMP 16 // 16-30C
 #define GREE_AC_FAN 0 // 1-3, 0 auto
